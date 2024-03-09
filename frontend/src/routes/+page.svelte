3acf2779f8bc518185474f3542693d1ac8ffe3d1
Francesco Manzali <manzali.francesco97@gmail.com>
2024-03-09 16:40:57 +0100

    Style fixes for experiment start page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -12,56 +12,52 @@
 	let formLoading = false;
 </script>
 
-<div class="space-y-2">
-	<p>Benvenuto/a all'esperimento!</p>
-	<p>Inserisci il codice per accedere:</p>
-</div>
+<div class="flex flex-col items-center justify-center space-y-2">
+	<h1 class="mb-6 text-3xl font-bold">Inizia l'Esperimento!</h1>
+	<p class="mb-6 text-lg">Inserisci il codice numerico che ti è stato fornito:</p>
 
-<div class="flex flex-col">
-	<div class="flex items-center">
-		<form
-			method="post"
-			action="?/selectExperiment"
-			use:enhance={() => {
-				formLoading = true;
-				return async ({ update }) => {
-					formLoading = false;
-					update();
-				};
-			}}
+	<form
+		method="post"
+		action="?/selectExperiment"
+		use:enhance={() => {
+			formLoading = true;
+			return async ({ update }) => {
+				formLoading = false;
+				update();
+			};
+		}}
+	>
+		<input
+			type="number"
+			class="mr-2 rounded-md border border-gray-300 px-4 py-2"
+			placeholder="Codice di accesso"
+			name="runCode"
+			disabled={formLoading}
+		/>
+		<button
+			class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+			type="submit"
+			disabled={formLoading}
 		>
-			<input
-				type="number"
-				class="mr-2 rounded-md border border-gray-300 px-4 py-2"
-				placeholder="Codice di accesso"
-				name="runCode"
-				disabled={formLoading}
-			/>
-			<button
-				class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-				type="submit"
-				disabled={formLoading}
-			>
-				Conferma
-			</button>
-		</form>
-	</div>
+			Conferma
+		</button>
+	</form>
 	{#if formLoading}
 		<Loader />
 	{/if}
-</div>
 
-{#if form?.error}
-	<div
-		class="relative mt-4 rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700"
-		role="alert"
-	>
-		<span class="block sm:inline">
-			{#if form.message == 'graphql-error'}
-				Qualcosa è andato storto, riprova più tardi.
-			{:else if form.message == 'run-not-found'}
-				Non esiste alcun esperimento con questo codice.
-			{/if}
-		</span>
-	</div>
-{/if}
+	{#if form?.error}
+		<div
+			class="relative mt-4 rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700"
+			role="alert"
+		>
+			<span class="block sm:inline">
+				{#if form.message == 'graphql-error'}
+					Qualcosa è andato storto, riprova più tardi.
+				{:else if form.message == 'run-not-found'}
+					Non esiste alcun esperimento con questo codice.
+				{/if}
+			</span>
+		</div>
+	{/if}
+</div>
