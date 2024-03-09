3e596e331cf2fc001445cd3d4f8b82ce0856741c
Francesco Manzali <manzali.francesco97@gmail.com>
2024-03-09 16:52:21 +0100

    Style fixes to experiment page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -12,13 +12,16 @@
 	let formLoading = false;
 </script>
 
-<div class="flex flex-col items-center justify-center space-y-2">
+<div class="flex flex-col items-center justify-center">
 	<h1 class="mb-6 text-3xl font-bold">Inizia l'Esperimento!</h1>
-	<p class="mb-6 text-lg">Inserisci il codice numerico che ti è stato fornito:</p>
+	<p class="mb-6 text-center text-lg">
+		Inserisci il codice numerico che ti è stato fornito per accedere all'esperimento:
+	</p>
 
 	<form
 		method="post"
 		action="?/selectExperiment"
+		class="w-full"
 		use:enhance={() => {
 			formLoading = true;
 			return async ({ update }) => {
@@ -27,20 +30,22 @@
 			};
 		}}
 	>
-		<input
-			type="number"
-			class="mr-2 rounded-md border border-gray-300 px-4 py-2"
-			placeholder="Codice di accesso"
-			name="runCode"
-			disabled={formLoading}
-		/>
-		<button
-			class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-			type="submit"
-			disabled={formLoading}
-		>
-			Conferma
-		</button>
+		<div class="flex flex-col">
+			<input
+				type="number"
+				class="mb-6 mr-2 w-full rounded-md border border-gray-300 px-4 py-2"
+				placeholder="Codice di accesso"
+				name="runCode"
+				disabled={formLoading}
+			/>
+			<button
+				class="m-auto rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+				type="submit"
+				disabled={formLoading}
+			>
+				Conferma
+			</button>
+		</div>
 	</form>
 	{#if formLoading}
 		<Loader />
